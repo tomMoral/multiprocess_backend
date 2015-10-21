a3c4030463270fd54cf6d0991058c781c346fc4e
Thomas Moreau <toma.moral@gmail.com>
2015-10-21 17:01:53 +0200

    Add MP_COMM_CHANNEL switch

--- spawn_client.py
+++ spawn_client.py
@@ -2,30 +2,39 @@
 import os
 import sys
 import dill as pickle
+from communication_channel import CommunicationChannel
 
 args = sys.argv
-assert "--pipe" in sys.argv
-argidx = sys.argv.index("--pipe")
-w = int(args[argidx+1])
-r = int(args[argidx+2])
-if sys.platform == 'win32':
-    import msvcrt
-    w = msvcrt.open_osfhandle(w, os.O_WRONLY)
-    r = msvcrt.open_osfhandle(r, os.O_RDONLY)
-child_write = os.fdopen(w, 'wb')
-child_read = os.fdopen(r, 'rb')
-queue_out = pickle.Pickler(child_write)
-queue_in = pickle.Unpickler(child_read)
-f = queue_in.load()
-args = queue_in.load()
+if "--pipe" in sys.argv:
+    argidx = sys.argv.index("--pipe")
+    w = int(args[argidx+1])
+    r = int(args[argidx+2])
+    if sys.platform == 'win32':
+        import msvcrt
+        w = msvcrt.open_osfhandle(w, os.O_WRONLY)
+        r = msvcrt.open_osfhandle(r, os.O_RDONLY)
+    chan = CommunicationChannel(w, r)
+else:
+    argidx = sys.argv.index("--port")
+    port = int(args[argidx+1])
+    address_in = ('localhost', port)
+    address_out = ('localhost', port+1)
+    from multiprocessing.connection import Listener, Client
+    listener = Listener(address_out, authkey=b'testingToms')
+    conn_in = Client(address_in, authkey=b'testingToms')
+    conn_out = listener.accept()
+    chan = CommunicationChannel(conn_out, conn_in)
+
+
+f = chan.load()
+args = chan.load()
 res = f(*args)
 print("Child writing", res)
-queue_out.dump("Text written by child...")
-queue_out.dump(res)
-child_write.flush()
+chan.dump("Text written by child...")
+chan.dump(res)
+#child_write.flush()
 
 # Clean up
-child_read.close()
-child_write.close()
+chan.close()
 print("Child closing")
 sys.exit(0)
--- test_subprocess.py
+++ test_subprocess.py
@@ -4,6 +4,7 @@ import os
 import sys
 import dill as pickle
 from subprocess import Popen
+from communication_channel import CommunicationChannel
 
 print("The child will write text to a pipe and ")
 print("the parent will read the text written by child...")
@@ -16,49 +17,66 @@ version = sys.version_info[:2]
 
 # Create the subprocess
 # and manage the pipes
-parent_r, child_w = os.pipe()
-child_r, parent_w = os.pipe()
+if ('MP_COMM_CHANNEL' in os.environ.keys() and
+        os.environ['MP_COMM_CHANNEL'] == 'pipe'):
+    parent_r, child_w = os.pipe()
+    child_r, parent_w = os.pipe()
 
-python_bin = '/usr/bin/python' + str(version[0])
-f_desc = [str(child_w), str(child_r)]
-if version >= (3, 3):
-    if sys.platform == 'win32':
-        # Change to Windwos file handle
+    python_bin = '/usr/bin/python' + str(version[0])
+    f_desc = [str(child_w), str(child_r)]
+    if version >= (3, 3):
+        if sys.platform == 'win32':
+            # Change to Windwos file handle
+            import msvcrt
+            child_wh = msvcrt.get_osfhandle(child_w)
+            child_rh = msvcrt.get_osfhandle(child_r)
+            os.set_handle_inheritable(child_wh, True)
+            os.set_handle_inheritable(child_rh, True)
+            python_bin = 'c:/python{}{}/python'.format(*version)
+            f_desc = [str(child_wh), str(child_rh)]
+        else:
+            os.set_inheritable(child_w, True)
+            os.set_inheritable(child_r, True)
+            f_desc = [str(child_w), str(child_r)]
+    elif sys.platform == 'win32':
         import msvcrt
         child_wh = msvcrt.get_osfhandle(child_w)
         child_rh = msvcrt.get_osfhandle(child_r)
-        os.set_handle_inheritable(child_wh, True)
-        os.set_handle_inheritable(child_rh, True)
         python_bin = 'c:/python{}{}/python'.format(*version)
         f_desc = [str(child_wh), str(child_rh)]
-    else:
-        os.set_inheritable(child_w, True)
-        os.set_inheritable(child_r, True)
-        f_desc = [str(child_w), str(child_r)]
-elif sys.platform == 'win32':
-    import msvcrt
-    child_wh = msvcrt.get_osfhandle(child_w)
-    child_rh = msvcrt.get_osfhandle(child_r)
-    python_bin = 'c:/python{}{}/python'.format(*version)
-    f_desc = [str(child_wh), str(child_rh)]
 
-proc = Popen([python_bin, 'spawn_client.py', '--pipe'] +
-             f_desc, close_fds=False)
-parent_w = os.fdopen(parent_w, 'wb')
-queue_out = pickle.Pickler(parent_w)
-parent_r = os.fdopen(parent_r, 'rb')
-queue_in = pickle.Unpickler(parent_r)
-os.close(child_w)
-os.close(child_r)
+    proc = Popen([python_bin, 'spawn_client.py', '--pipe'] +
+                 f_desc, close_fds=False)
+    os.close(child_w)
+    os.close(child_r)
+    chan = CommunicationChannel(parent_w, parent_r)
+elif ('MP_COMM_CHANNEL' in os.environ.keys() and
+        os.environ['MP_COMM_CHANNEL'] == 'conn'):
+    from multiprocessing.connection import Listener, Client
+
+    python_bin = '/usr/bin/python' + str(version[0])
+    port = 42027
+    address_out = ('localhost', port)
+    address_in = ('localhost', port+1)
+    proc = Popen([python_bin, 'spawn_client.py', '--port'] +
+                 [str(port)], close_fds=False)
+    listener = Listener(address_out, authkey=b'testingToms')
+    conn_out = listener.accept()
+    print('Connection accepted from', listener.last_accepted)
+    conn_in = Client(address_in, authkey=b'testingToms')
+
+    chan = CommunicationChannel(conn_out, conn_in)
+else:
+    raise NotImplementedError('No such connector')
 
 # This is the parent process
-queue_out.dump(test)
-queue_out.dump((27, 42))
-parent_w.flush()
+chan.dump(test)
+chan.dump((27, 42))
+#parent_w.flush()
 print("Parent sleeping or doing stuff")
 #time.sleep(.5)
-msg = queue_in.load()
-res = queue_in.load()
+msg = chan.load()
+res = chan.load()
 print(msg)
 print("Result =", res)
 sys.exit(0)
