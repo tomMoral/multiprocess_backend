f9155ec296a695fa1cb160c02ba2bf49c7900b76
Thomas Moreau <toma.moral@gmail.com>
2015-10-21 17:04:17 +0200

    Add communication channel

---
+++ communication_channel.py
@@ -0,0 +1,45 @@
+import os
+import dill as pickle
+
+
+if ('MP_COMM_CHANNEL' in os.environ.keys() and
+        os.environ['MP_COMM_CHANNEL'] == 'conn'):
+    class CommunicationChannel(object):
+        '''Bi directional communication channel
+        '''
+        def __init__(self, conn_out, conn_in):
+            self.conn_out = conn_out
+            self.conn_in = conn_in
+
+        def close(self):
+            self.conn_in.close()
+            self.conn_out.close()
+
+        def dump(self, obj):
+            self.conn_out.send_bytes(pickle.dumps(obj))
+
+        def load(self):
+            return pickle.loads(self.conn_in.recv_bytes())
+elif ('MP_COMM_CHANNEL' in os.environ.keys() and
+        os.environ['MP_COMM_CHANNEL'] == 'pipe'):
+    class CommunicationChannel(object):
+        '''Bi directional communication channel
+        '''
+        def __init__(self, fdw, fdr):
+            self.fdw = os.fdopen(fdw, 'wb')
+            self.fdr = os.fdopen(fdr, 'rb')
+            self.queue_out = pickle.Pickler(self.fdw)
+            self.queue_in = pickle.Unpickler(self.fdr)
+
+        def close(self):
+            self.fdr.close()
+            self.fdw.close()
+
+        def dump(self, obj):
+            self.queue_out.dump(obj)
+            self.fdw.flush()
+
+        def load(self):
+            return self.queue_in.load()
+else:
+    raise NotImplementedError('Wrong backend')
